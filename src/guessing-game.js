c95e79e946e289748fc4578780efbae11b194d72
Ivan Kulakov <ivankulakov@MacBook-Pro-Ivan.local>
2022-08-10 19:35:31 +0300

    feat: add impl

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        this.middle = Math.round((this.max - this.min)/2 + this.min);
+      
 
+
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
+        this.guess();
     }
 
     greater() {
-
+        this.min = this.middle;
+        this.guess();
     }
 }
 
